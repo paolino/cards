c657102cf4bacf2b21501e15ce9ba42b106a3ed4
paolo <paolo.veronelli@gmail.com>
2023-03-10 16:17:55 +0000

    make target a parameter

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,12 @@
 
 module Main (main) where
 
-import Control.Monad
+import Control.Monad ((<=<))
 import Lib qualified
 import Streaming.Prelude qualified as S
-
-target :: String
-target = "https://data.scryfall.io/all-cards/all-cards-20230310101654.json"
+import System.Environment (getArgs)
 
 main :: IO ()
-main = Lib.main target (print <=< S.length_)
+main = do
+    (target : _) <- getArgs
+    Lib.main target $ print <=< S.length_
