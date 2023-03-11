d64bc49937d68eee8de4703963361a911a02d16b
paolo <paolo.veronelli@gmail.com>
2023-03-11 14:26:48 +0000

    add fourmolu conf add some docs change main name to run

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,11 @@
 module Main (main) where
 
 import Control.Monad ((<=<))
-import Lib qualified
+import Lib ( run )
 import Streaming.Prelude qualified as S
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
     (target : _) <- getArgs
-    Lib.main target $ print <=< S.length_
+    run target $ print <=< S.length_
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,65 +4,80 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Lib (main) where
+module Lib (run) where
 
-import Data.ByteString.Streaming.HTTP (
-    Response (responseBody),
-    newManager,
-    parseRequest,
-    tlsManagerSettings,
-    withHTTP,
- )
+import Data.ByteString.Streaming.HTTP
+    ( Response (responseBody)
+    , newManager
+    , parseRequest
+    , tlsManagerSettings
+    , withHTTP
+    )
 import Streaming.ByteString qualified as Q
 
-import Control.Monad.State.Strict (
-    MonadState (get, put),
-    StateT,
-    evalStateT,
- )
+import Control.Monad.State.Strict
+    ( MonadState (get, put)
+    , StateT
+    , evalStateT
+    )
 import Data.Aeson (Value)
 import Data.ByteString (ByteString)
-import Data.JsonStream.Parser (
-    ParseOutput (..),
-    Parser,
-    arrayOf,
-    runParser,
-    value,
- )
-import Streaming (
-    MFunctor (hoist),
-    MonadTrans (lift),
-    Of,
-    Stream,
-    distribute,
- )
+import Data.JsonStream.Parser
+    ( ParseOutput (..)
+    , Parser
+    , arrayOf
+    , runParser
+    , value
+    )
+import Streaming
+    ( MFunctor (hoist)
+    , MonadTrans (lift)
+    , Of
+    , Stream
+    , distribute
+    )
 import Streaming.Prelude qualified as S
 
-parseJson ::
-    forall m a.
-    Monad m =>
-    Parser a ->
-    Stream (Of ByteString) m () ->
-    Stream (Of a) m ()
+-- | Parse a stream of ByteStings into values via a streaming json parser.
+parseJson
+    :: forall m a
+     . Monad m
+    => Parser a
+    -- ^ Parser to use
+    -> Stream (Of ByteString) m ()
+    -- ^ Stream of chunks to consume
+    -> Stream (Of a) m ()
+    -- ^ Stream of parsed values
 parseJson parser =
     flip evalStateT (runParser parser)
         . distribute
         . flip S.for oneChunk
         . hoist lift
 
-oneChunk :: Monad m => ByteString -> Stream (Of a) (StateT (ParseOutput a) m) ()
+-- | Parse a ByteString into some JSON, keeping track of the parser state.
+oneChunk
+    :: Monad m
+    => ByteString
+    -- ^ Chunk of input to consume
+    -> Stream (Of a) (StateT (ParseOutput a) m) ()
+    -- ^ Stream of parsed values with updated parser state
 oneChunk x = do
     let loop p = case p of
             ParseDone r -> case r of
                 "" -> return ()
                 _ -> error "parseJson: leftover"
             ParseFailed e -> error e
-            ParseYield a c -> S.yield a >> loop c
+            ParseYield a p' -> S.yield a >> loop p'
             ParseNeedData c -> put $ c x
     get >>= loop
 
-main :: String -> (Stream (Of Value) IO () -> IO ()) -> IO ()
-main target what = do
+run
+    :: String
+    -- ^ Target URL
+    -> (Stream (Of Value) IO () -> IO ())
+    -- ^ What to do with the stream
+    -> IO ()
+run target what = do
     req <- parseRequest target
     m <- newManager tlsManagerSettings
     withHTTP req m $ \resp ->
